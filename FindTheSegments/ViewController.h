b4a00ecc434f80c4cc907a9bd0c2dd35b59b191d
Max Woolf <maximus@zingysaturn.co.uk>
2014-11-10 10:34:43 +0000

    layout

--- FindTheSegments/ViewController.h
+++ FindTheSegments/ViewController.h
@@ -1,15 +1,9 @@
-//
-//  ViewController.h
-//  FindTheSegments
-//
-//  Created by Max Woolf on 10/11/2014.
-//  Copyright (c) 2014 maxwoolf. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
+@interface ViewController : UIViewController <CLLocationManagerDelegate>
 
-@interface ViewController : UIViewController
-
+@property (nonatomic, strong) CLLocationManager *locationManager;
+@property (nonatomic, strong) CLBeaconRegion *beaconRegion;
 
 @end
 
--- FindTheSegments/ViewController.m
+++ FindTheSegments/ViewController.m
@@ -1,11 +1,3 @@
-//
-//  ViewController.m
-//  FindTheSegments
-//
-//  Created by Max Woolf on 10/11/2014.
-//  Copyright (c) 2014 maxwoolf. All rights reserved.
-//
-
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -16,7 +8,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:@"74278bda-b644-4520-8f0c-720eaf059935"]; // Definitely need to change this when beacons arrive
+    _beaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID:uuid
+                                                       identifier:@"com.substrakt.findthesegments"];
+    _beaconRegion.notifyEntryStateOnDisplay = YES;
+    _beaconRegion.notifyOnEntry = YES;
+    [_locationManager setDelegate:self];
+    [_locationManager requestAlwaysAuthorization];
+    [_locationManager startMonitoringForRegion:_beaconRegion];
 }
 
 - (void)didReceiveMemoryWarning {
