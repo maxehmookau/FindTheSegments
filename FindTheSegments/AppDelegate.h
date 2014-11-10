7eb566ad5f1d2b77a40fa475f9f4a91c9bb51ea7
Max Woolf <maximus@zingysaturn.co.uk>
2014-11-10 10:11:57 +0000

    Initial Commit

---
+++ FindTheSegments/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FindTheSegments
+//
+//  Created by Max Woolf on 10/11/2014.
+//  Copyright (c) 2014 maxwoolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FindTheSegments/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FindTheSegments
+//
+//  Created by Max Woolf on 10/11/2014.
+//  Copyright (c) 2014 maxwoolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
