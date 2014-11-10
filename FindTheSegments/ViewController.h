1da674475bd20852f4bcb2c2dfbcf26de9e79f44
Max Woolf <maximus@zingysaturn.co.uk>
2014-11-10 12:59:30 +0000

    add everything and reset button

--- FindTheSegments/ViewController.h
+++ FindTheSegments/ViewController.h
@@ -1,9 +1,24 @@
 #import <UIKit/UIKit.h>
 #import <CoreLocation/CoreLocation.h>
+#import <AVFoundation/AVFoundation.h>
 @interface ViewController : UIViewController <CLLocationManagerDelegate>
 
 @property (nonatomic, strong) CLLocationManager *locationManager;
 @property (nonatomic, strong) CLBeaconRegion *beaconRegion;
+
+@property (nonatomic) IBOutlet UIView *blueBeaconView;
+@property (nonatomic) IBOutlet UIView *redBeaconView;
+@property (nonatomic) IBOutlet UIView *greenBeaconView;
+@property (nonatomic) IBOutlet UIView *yellowBeaconView;
+
+@property (nonatomic) AVAudioPlayer *player;
+
+@property (nonatomic, strong) NSMutableDictionary *appState;
+
+- (void)didStandImmediatelyNextToBeacon:(CLBeacon *)beacon;
+- (void)updateView;
+
+- (IBAction)didPressResetButton:(id)sender;
 
 @end
 
--- FindTheSegments/ViewController.m
+++ FindTheSegments/ViewController.m
@@ -8,6 +8,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSURL *url = [NSURL fileURLWithPath:[[NSBundle mainBundle]
+                                         pathForResource:@"collect"
+                                         ofType:@"WAV"]];
+    [self updateView];
+    NSError *error = nil;
+    _player = [[AVAudioPlayer alloc]
+                                   initWithContentsOfURL:url
+                                   error:&error];
+    _appState = [@{@1: @NO,
+                   @2: @NO,
+                   @3: @NO,
+                   @4: @NO} mutableCopy];
+    _locationManager = [CLLocationManager new];
     NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:@"74278bda-b644-4520-8f0c-720eaf059935"]; // Definitely need to change this when beacons arrive
     _beaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID:uuid
                                                        identifier:@"com.substrakt.findthesegments"];
@@ -18,9 +31,84 @@
     [_locationManager startMonitoringForRegion:_beaconRegion];
 }
 
+#pragma mark - CLLocationManagerDelegate
+
+- (void)locationManager:(CLLocationManager *)manager didDetermineState:(CLRegionState)state forRegion:(CLRegion *)region {
+    switch (state) {
+        case CLRegionStateInside:
+            [_locationManager startRangingBeaconsInRegion:_beaconRegion];
+            break;
+            
+        default:
+            break;
+    }
+}
+
+- (void)locationManager:(CLLocationManager *)manager didEnterRegion:(CLRegion *)region {
+    [_locationManager startRangingBeaconsInRegion:(CLBeaconRegion *)region];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didRangeBeacons:(NSArray *)beacons inRegion:(CLBeaconRegion *)region {
+    NSLog(@"%@", beacons);
+    for (CLBeacon *beacon in beacons) {
+        if (beacon.proximity == CLProximityImmediate) {
+            [self didStandImmediatelyNextToBeacon:beacon];
+        }
+    }
+}
+
+- (void)didStandImmediatelyNextToBeacon:(CLBeacon *)beacon {
+    if ([_appState[beacon.minor] isEqual:@NO]) {
+        [_player play];
+    }
+    _appState[beacon.minor] = @YES;
+    NSLog(@"Stood next to a beacon");
+    NSLog(@"%@", _appState);
+    [self updateView];
+//    AVSpeechUtterance *speech = [[AVSpeechUtterance alloc] initWithString:@"You are stood next to beacon with minor number 3"];
+//    AVSpeechSynthesizer *synth = [[AVSpeechSynthesizer alloc] init];
+//    [synth speakUtterance:speech];
+}
+
+- (void)updateView {
+    if ([_appState[@1] isEqual:@YES]) {
+        _redBeaconView.backgroundColor = [UIColor redColor];
+    } else {
+        _redBeaconView.backgroundColor = [UIColor colorWithRed:1 green:0 blue:0 alpha:0.3];
+    }
+    
+    if ([_appState[@4] isEqual:@YES]) {
+        _blueBeaconView.backgroundColor = [UIColor blueColor];
+    } else {
+        _blueBeaconView.backgroundColor = [UIColor colorWithRed:0 green:0 blue:1 alpha:0.3];
+    }
+    
+    if ([_appState[@5] isEqual:@YES]) {
+        _yellowBeaconView.backgroundColor = [UIColor yellowColor];
+    } else {
+        _yellowBeaconView.backgroundColor = [UIColor colorWithRed:1 green:1 blue:0 alpha:0.3];
+    }
+
+    if ([_appState[@2] isEqual:@YES]) {
+        _greenBeaconView.backgroundColor = [UIColor greenColor];
+    } else {
+        _greenBeaconView.backgroundColor = [UIColor colorWithRed:0 green:1 blue:0 alpha:0.3];
+    }
+    
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)didPressResetButton:(id)sender {
+    NSLog(@"Reset");
+    _appState = [@{@1: @NO,
+                  @2: @NO,
+                  @3: @NO,
+                  @4: @NO} mutableCopy];
+    [self updateView];
 }
 
 @end
